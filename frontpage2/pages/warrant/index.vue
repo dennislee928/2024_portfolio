5044582b0ea30b498a53dd687d4f99021f22274f
Bityacht-Developer <55692615+dennislee928@users.noreply.github.com>
2024-05-20 14:27:24 +0800

    add:warrant page

---
+++ frontpage2/pages/warrant/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <NewHeader />
+  <!-- First Divider -->
+  <div class="divider"></div>
+  <div class="title">
+    <h1 class="first-title">Dennis Lee</h1>
+
+    <h3 class="second-title">Savior or Manic?</h3>
+  </div>
+  <!-- Second Divider -->
+  <div class="divider"></div>
+
+  <!-- Third Divider -->
+  <div class="divider"></div>
+</template>
